71789cf28a42c399a861a20d13d9f3e44a46dff2
satyamedh hulyalkar <47636284+satyamedh@users.noreply.github.com>
2023-10-08 12:19:26 +0530

    Added get node and get neighbours functions

--- classes/grid.py
+++ classes/grid.py
@@ -20,3 +20,23 @@ class AStarGrid:
                                         Location(x, y) == self.END))
                 counter += 1
         return new_grid
+
+    def get_node(self, location: Location):
+        # check if the location is valid
+        if 0 <= location.X < len(self.GRID[0]) and 0 <= location.Y < len(self.GRID):
+            return self.GRID[location.Y][location.X]
+        else:
+            raise Exception(f"Location {location} is not valid")
+
+    def get_neighbors(self, node: Node):
+        neighbors = []
+        # Get the neighbors of the node
+        for y in range(node.LOCATION.Y - 1, node.LOCATION.Y + 2):
+            for x in range(node.LOCATION.X - 1, node.LOCATION.X + 2):
+                # Get the current node
+                current_node = self.get_node(Location(x, y))
+                # Check if the node is valid, not an obstacle, and not the current node
+                if (0 <= x < len(self.GRID[0]) and 0 <= y < len(self.GRID)
+                        and current_node != node and not current_node.OBSTACLE):
+                    neighbors.append(current_node)
+        return neighbors
